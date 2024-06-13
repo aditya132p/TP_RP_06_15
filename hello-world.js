5be5bd80c5a06beb2be7e3967865a461eef28869
aditya pawar <99139758+aditya132p@users.noreply.github.com>
2024-06-13 17:59:34 +0530

    Add files via upload

---
+++ hello-world.js
@@ -0,0 +1,17 @@
+console.log("hello world")
+
+var a = "Hello World";
+
+for ( let i=0; i<5; i++){
+    console.log(i,a)
+}
+
+// OR
+
+console.log(15+15)
+console.log(85+17)
+console.log(45+15)
+console.log(85+25)
+console.log(50+15)
+
+
